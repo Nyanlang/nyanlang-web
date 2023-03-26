b90e9f0473b51163971207518d7065c4fadd13c7
sserve-kr <personal@sserve.me>
2023-03-26 15:41:25 +0900

    Improved theme support

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -9,19 +9,23 @@ import {
     Flex,
     Heading,
     Link,
-    IconButton
+    IconButton, useColorMode, useColorModeValue,
+    Icon
 } from "@chakra-ui/react";
 import React from "react";
 import {useRouter} from "next/router";
-import {CloseIcon, HamburgerIcon} from "@chakra-ui/icons";
+import {CloseIcon, HamburgerIcon, MoonIcon, SunIcon} from "@chakra-ui/icons";
+import {AiFillGithub} from "react-icons/ai"
 import {useState} from "react";
 
 
 function NavigationLink(prop: {href: string, children: React.ReactNode}) {
     let router = useRouter();
 
+    const navTextColor = useColorModeValue("gray.800", "whiteAlpha.900");
+
     return (
-        <Link href={prop.href} fontSize={"lg"} fontWeight={"semibold"} mx={"10px"} color={router.route === prop.href ? "nyanlang.600" : "gray.800"} _hover={{textDecoration:"none",color:"nyanlang.500"}}>{prop.children}</Link>
+        <Link href={prop.href} fontSize={"lg"} fontWeight={"semibold"} mx={"10px"} color={router.route === prop.href ? "nyanlang.600" : navTextColor} _hover={{textDecoration:"none",color:"nyanlang.500"}}>{prop.children}</Link>
     )
 }
 
@@ -34,6 +38,9 @@ function NavigationExternalBtn(prop: {onClick: Function, children: React.ReactNo
 export default function Navigation() {
     let router = useRouter();
 
+    const {colorMode, toggleColorMode} = useColorMode();
+    const navBgColor = useColorModeValue("whiteAlpha.900", "gray.900");
+
     return (
         <Flex
             direction={"row"}
@@ -41,7 +48,7 @@ export default function Navigation() {
             align={"center"}
             h={"80px"}
             w={"100vw"}
-            bg={"whiteAlpha.900"}
+            bg={navBgColor}
             boxShadow={"md"}
             position={"fixed"}
             zIndex={100}
@@ -71,9 +78,10 @@ export default function Navigation() {
                 </Flex>
                 <Flex direction={"row"} justify={"center"} align={"center"} h={"100%"}>
                     <NavigationExternalBtn onClick={() => router.push("https://github.com/nyanlang/nyanlang")}>
-                        <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.475 2 2 6.475 2 12C2 16.425 4.8625 20.1625 8.8375 21.4875C9.3375 21.575 9.525 21.275 9.525 21.0125C9.525 20.775 9.5125 19.9875 9.5125 19.15C7 19.6125 6.35 18.5375 6.15 17.975C6.0375 17.6875 5.55 16.8 5.125 16.5625C4.775 16.375 4.275 15.9125 5.1125 15.9C5.9 15.8875 6.4625 16.625 6.65 16.925C7.55 18.4375 8.9875 18.0125 9.5625 17.75C9.65 17.1 9.9125 16.6625 10.2 16.4125C7.975 16.1625 5.65 15.3 5.65 11.475C5.65 10.3875 6.0375 9.4875 6.675 8.7875C6.575 8.5375 6.225 7.5125 6.775 6.1375C6.775 6.1375 7.6125 5.875 9.525 7.1625C10.325 6.9375 11.175 6.825 12.025 6.825C12.875 6.825 13.725 6.9375 14.525 7.1625C16.4375 5.8625 17.275 6.1375 17.275 6.1375C17.825 7.5125 17.475 8.5375 17.375 8.7875C18.0125 9.4875 18.4 10.375 18.4 11.475C18.4 15.3125 16.0625 16.1625 13.8375 16.4125C14.2 16.725 14.5125 17.325 14.5125 18.2625C14.5125 19.6 14.5 20.675 14.5 21.0125C14.5 21.275 14.6875 21.5875 15.1875 21.4875C17.1727 20.8173 18.8977 19.5415 20.1198 17.8395C21.3419 16.1376 21.9995 14.0953 22 12C22 6.475 17.525 2 12 2Z" fill="#000000"/>
-                        </svg>
+                        <Icon as={AiFillGithub} w={6} h={6} />
+                    </NavigationExternalBtn>
+                    <NavigationExternalBtn onClick={() => toggleColorMode()}>
+                        {colorMode === "light" ? <MoonIcon w={6} h={6} /> : <SunIcon w={6} h={6} />}
                     </NavigationExternalBtn>
                 </Flex>
             </Flex>
@@ -84,8 +92,10 @@ export default function Navigation() {
 function DocsNavigationItem(props: {href: string, children: React.ReactNode}) {
     let router = useRouter();
 
+    const navTextColor = useColorModeValue("black", "whiteAlpha.900");
+
     return <Box w={"100%"} fontWeight={"thin"} py={"3px"} >
-        <Link href={props.href} color={router.route === props.href ? "nyanlang.700" : "black"} _hover={{textDecoration:"none",color:"nyanlang.500"}}>{props.children}</Link>
+        <Link href={props.href} color={router.route === props.href ? "nyanlang.700" : navTextColor} _hover={{textDecoration:"none",color:"nyanlang.500"}}>{props.children}</Link>
     </Box>
 }
 
@@ -108,9 +118,11 @@ function DocsNavigationCategory(props: {title: string, children: React.ReactNode
 export function DocsNavigation() {
     let [isOpen, setIsOpen] = useState(false)
 
+    const navBgColor = useColorModeValue("white", "gray.900");
+
     return <>
         <IconButton aria-label={"메뉴"} icon={isOpen ? <CloseIcon /> : <HamburgerIcon />} size={"md"} display={["block", null, null, "none"]} position={"fixed"} top={"90px"} left={"10px"} onClick={() => setIsOpen((prev) => !prev)} zIndex={100} />
-        <Accordion overflowY={"auto"} allowToggle display={[isOpen ? "block" : "none", null, null, "block"]} position={["relative", null, null, "sticky"]} top={"0px"} w={["100vw", null, null, "100%"]} h={["100vh", null, null, "fit-content"]} bg={"white"} pt={["80px", null, null, "5px"]}>
+        <Accordion overflowY={"auto"} allowToggle display={[isOpen ? "block" : "none", null, null, "block"]} position={["relative", null, null, "sticky"]} top={"0px"} w={["100vw", null, null, "100%"]} h={["100vh", null, null, "fit-content"]} bg={navBgColor} pt={["80px", null, null, "5px"]}>
             <DocsNavigationCategory title={"빠르게 시작하기"}>
                 <DocsNavigationItem href={"/docs/getting-started#installation"}>설치</DocsNavigationItem>
                 <DocsNavigationItem href={"/docs/getting-started#hello-world"}>Hello World</DocsNavigationItem>
